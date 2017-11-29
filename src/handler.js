2a4b58ff57885c826537bbab50d9a287c700e868
sophielevens <sophielevens1@gmail.com>
2017-11-29 13:01:41 +0000

    created skeleton for front end and back end relates #1

---
+++ src/handler.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
+
+const homeHandler = (request, response) => {
+
+}
+
+const staticHandler = (request, response, endpoint) => {
+
+}
+
+const addMe = (request, response, endpoint) => {
+
+}
+
+const viewAll = (request, response, endpoint) => {
+
+}
+
+module.exports = {
+  homeHandler,
+  staticHandler,
+  addMe,
+  viewAll
+}
