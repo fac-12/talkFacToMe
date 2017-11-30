1060052aa7d6e926f2bf2a9d049e57007ee41abf
sophielevens <sophielevens1@gmail.com>
2017-11-30 11:53:12 +0000

    deployed to heroku and initialised eslint

--- database/db_connections.js
+++ database/db_connections.js
@@ -2,16 +2,16 @@
 const { Pool } = require('pg');
 const url = require('url');
 // config.env is used to store sensitive information
-require('env2')('./config.env');
+require ('env2')('./config.env');
 
-let !DB_URL = process.env.DB_URL;
-if(process.env.NODE.ENV === 'test'){
-    DB_URL = process.env.TEST_DB_URL;
+let DATABASE_URL = process.env.DATABASE_URL;
+if (process.env.NODE_ENV === 'test'){
+    DATABASE_URL = process.env.TEST_DATABASE_URL;
 }
 
-if(!DB_URL) throw new Error('Environment variable DB_URL must be set');
+if (!DATABASE_URL) throw new Error('Environment variable DATABASE_URL must be set');
 
-const params = url.parse(DB_URL);
+const params = url.parse(DATABASE_URL);
 
 //create one array containing username and password in the url
 const [username, password] = params.auth.split(':');
@@ -24,7 +24,7 @@ const options = {
     max: process.env.DB_MAX_CONNECTIONS || 2,
     user: username,
     password,
-    
+
     // if you want to access a remote server use ssl (a secure connection) otherwise use local host
     ssl: params.hostname !== 'localhost',
 };
