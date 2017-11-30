9cf39fa7fe94c3e0a8e6556095cfc4bad4844f75
Gbaja <gbajaf@yahoo.co.uk>
2017-11-30 16:56:03 +0000

    Made it look pretty

--- public/app.js
+++ public/app.js
@@ -5,6 +5,9 @@ console.log("hello world");
 var viewAllBtn = document.getElementById('viewAllBtn');
 var displayPeople = document.getElementById('displayPeople');
 var aboutSection = document.getElementById('about');
+var joinDatabaseBtn = document.getElementById('joindatabase');
+
+
 function request(url, cb) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
@@ -53,3 +56,24 @@ homeBtn.addEventListener("click", function() {
   aboutSection.style.display = "block";
   displayPeople.style.display = "none";
 })
+
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+joinDatabaseBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
