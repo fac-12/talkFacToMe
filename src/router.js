29c7f40cb17b26568b18aea68c0df9f9a4ab2211
Johanna-hub <jyherman@gmail.com>
2017-12-08 12:07:41 +0000

    all routes protected relates to #93

--- src/router.js
+++ src/router.js
@@ -3,6 +3,24 @@ const secret = process.env.secret;
 const jwt = require('jsonwebtoken');
 const cookie = require('cookie');
 
+const cookieCheck = (request, response, endpoint, cb) => {
+if(request.headers.cookie){
+var userJwt = cookie.parse(request.headers.cookie);
+console.log('userjwt', userJwt);
+jwt.verify(userJwt.Token, secret, (err, decoded) => {
+  if (err){
+    response.writeHead(401);
+    response.end("Sorry, you don't have permission to view this page")
+  } else {
+    cb(request, response, endpoint)
+  }
+})
+} else {
+response.writeHead(401);
+response.end("Sorry, you don't have permission to view this page")
+}
+}
+
 const router = (request, response) => {
   const endpoint = request.url;
   if (endpoint === '/') {
@@ -12,49 +30,34 @@ const router = (request, response) => {
     staticHandler(request, response, endpoint)
   }
   else if (endpoint.indexOf('/addMe') !== -1) {
-    console.log("Index of is on");
-    addMe(request, response, endpoint);
+    cookieCheck(request, response, endpoint, addMe);
   }
   else if (endpoint.indexOf('/viewAll') !== -1){
-    if(request.headers.cookie){
-    var userJwt = cookie.parse(request.headers.cookie);
-    console.log('userjwt', userJwt);
-    jwt.verify(userJwt.Token, secret, (err, decoded) => {
-      if (err){
-        response.writeHead(401);
-        response.end("Sorry, you don't have permission to view this page")
-      } else {
-        viewAll(request, response, endpoint)
-      }
-    })
-  } else {
-    response.writeHead(401);
-    response.end("Sorry, you don't have permission to view this page")
-  }
+    cookieCheck(request, response, endpoint, viewAll);
   }
   else if(endpoint.indexOf('/fac') !== -1){
-    facLife(request, response, endpoint)
+    cookieCheck(request, response, endpoint, facLife);
   }
   else if(endpoint.indexOf('/freelance') !== -1){
-    freelance(request, response, endpoint)
+    cookieCheck(request, response, endpoint, freelance);
   }
   else if(endpoint.indexOf('/internship') !== -1){
-    internship(request, response, endpoint)
+    cookieCheck(request, response, endpoint, internship);
   }
   else if(endpoint.indexOf('/juniordev') !== -1){
-    juniorDev(request, response, endpoint)
+    cookieCheck(request, response, endpoint, juniorDev);
   }
   else if(endpoint.indexOf('/mentor') !== -1){
-    mentor(request, response, endpoint)
+    cookieCheck(request, response, endpoint, mentor);
   }
   else if(endpoint.indexOf('/register') !== -1){
-    register(request, response, endpoint)
+    cookieCheck(request, response, endpoint, register);
   }
   else if(endpoint.indexOf('/login') !== -1){
     login(request, response, endpoint);
   }
   else if(endpoint.indexOf('/logout') !== -1){
-    logout(request, response, endpoint);
+    cookieCheck(request, response, endpoint, logout);
   }
   else {
     response.writeHead(404, {"Content-Type": "text/plain"});
